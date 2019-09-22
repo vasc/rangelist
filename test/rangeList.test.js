962844e18fe44190f5438b7a5f71bc81b4fa6a7f
Vasco Fernandes <vasco.box@gmail.com>
2019-09-22 10:58:06 +0200

    Allow removing ranges from a RangeList
    Adds tests and implementation of the remove method in RangeList

--- test/rangeList.test.js
+++ test/rangeList.test.js
@@ -49,3 +49,47 @@ test("adding an empty range has no effect", () => {
   expect(rl.toString()).toBe("[1, 5)");
 });
 
+test("removing invalid ranges throws an error", () => {
+  expect(() => rl.remove([])).toThrow();
+  expect(() => rl.remove([5, 2])).toThrow();
+});
+
+test("removing the same range from a range list makes the list empty", () => {
+  rl.add([1, 5]);
+  rl.remove([1, 5]);
+  expect(rl.toString()).toBe("[)");
+});
+
+test("removing a range from the left shortens the range", () => {
+  rl.add([1, 5]);
+  rl.remove([1, 3]);
+  expect(rl.toString()).toBe("[3, 5)");
+});
+
+test("removing a range from the right shortens the range", () => {
+  rl.add([1, 5]);
+  rl.remove([3, 5]);
+  expect(rl.toString()).toBe("[1, 3)");
+});
+
+test("removing an empty range has no effect", () => {
+  rl.add([1, 5]);
+  rl.remove([3, 3]);
+  expect(rl.toString()).toBe("[1, 5)");
+});
+
+test("removing a range overlapping all existing ranges makes the list empty", () => {
+  rl.add([1, 5]);
+  rl.add([10, 20]);
+  rl.remove([0, 100]);
+
+  expect(rl.toString()).toBe("[)");
+});
+
+test("removing a range in the middle of an existing range splits the existing one in 2", () => {
+  rl.add([1, 10]);
+  rl.remove([3, 5]);
+
+  expect(rl.toString()).toBe("[1, 3) [5, 10)");
+});
+
