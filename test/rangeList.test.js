4e8c5fdfd4a4a99f7f21f4d525e55902042cb0bc
Vasco Fernandes <vasco.box@gmail.com>
2019-09-22 11:00:09 +0200

    Add tests that verify task examples/requirements

--- test/rangeList.test.js
+++ test/rangeList.test.js
@@ -93,3 +93,36 @@ test("removing a range in the middle of an existing range splits the existing on
   expect(rl.toString()).toBe("[1, 3) [5, 10)");
 });
 
+test("task examples execute correctly", () => {
+  expect(rl.ranges).toStrictEqual([]);
+
+  rl.add([1, 5]);
+  expect(rl.toString()).toBe("[1, 5)");
+
+  rl.add([10, 20]);
+  expect(rl.toString()).toBe("[1, 5) [10, 20)");
+
+  rl.add([20, 20]);
+  expect(rl.toString()).toBe("[1, 5) [10, 20)");
+
+  rl.add([20, 21]);
+  expect(rl.toString()).toBe("[1, 5) [10, 21)");
+
+  rl.add([2, 4]);
+  expect(rl.toString()).toBe("[1, 5) [10, 21)");
+
+  rl.add([3, 8]);
+  expect(rl.toString()).toBe("[1, 8) [10, 21)");
+
+  rl.remove([10, 10]);
+  expect(rl.toString()).toBe("[1, 8) [10, 21)");
+
+  rl.remove([10, 11]);
+  expect(rl.toString()).toBe("[1, 8) [11, 21)");
+
+  rl.remove([15, 17]);
+  expect(rl.toString()).toBe("[1, 8) [11, 15) [17, 21)");
+
+  rl.remove([3, 19]);
+  expect(rl.toString()).toBe("[1, 3) [19, 21)");
+});
