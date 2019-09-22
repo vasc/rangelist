08958a84ce583da6fb87b7871db03d8c581ceb67
Vasco Fernandes <vasco.box@gmail.com>
2019-09-22 10:55:31 +0200

    Allow adding ranges to the list
    Tests and implementation of the add method for the RangeList class

--- test/rangeList.test.js
+++ test/rangeList.test.js
@@ -9,3 +9,43 @@ test("empty range list is empty", () => {
   expect(rl.toString()).toBe("[)");
 });
 
+test("adding invalid ranges throws an error", () => {
+  expect(() => rl.add([])).toThrow();
+  expect(() => rl.add([5, 2])).toThrow();
+});
+
+test("adding a single range to the list displays that range", () => {
+  rl.add([1, 5]);
+  expect(rl.toString()).toBe("[1, 5)");
+});
+
+test("adding a 2nd range to a list shows a list with 2 ranges", () => {
+  rl.add([1, 5]);
+  rl.add([10, 20]);
+  expect(rl.toString()).toBe("[1, 5) [10, 20)");
+});
+
+test("adding overlapping ranges returns 1 range", () => {
+  rl.add([1, 5]);
+  rl.add([5, 10]);
+  expect(rl.toString()).toBe("[1, 10)");
+});
+
+test("adding an overlaping range to the right, extends the range", () => {
+  rl.add([1, 5]);
+  rl.add([5, 7]);
+  expect(rl.toString()).toBe("[1, 7)");
+});
+
+test("adding an overlaping range to the left, extends the range", () => {
+  rl.add([1, 5]);
+  rl.add([-4, 1]);
+  expect(rl.toString()).toBe("[-4, 5)");
+});
+
+test("adding an empty range has no effect", () => {
+  rl.add([1, 5]);
+  rl.add([10, 10]);
+  expect(rl.toString()).toBe("[1, 5)");
+});
+
